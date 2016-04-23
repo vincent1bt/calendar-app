c3435d511186c34bd2f237a0c0e381e28d1b53b2
Vicente <vincent_97@live.com.mx>
2016-04-23 15:33:07 -0500

    agregadas las dependencias y el Procfile

--- app.go
+++ app.go
@@ -2,11 +2,17 @@ package main
 
 import (
   "log"
+  "os"
   "net/http"
   "projects/calendarApp/routes"
 )
 
 func main() {
   router := routes.NewRouter()
-  log.Fatal(http.ListenAndServe(":8080", router))
+  port := os.Getenv("PORT")
+
+  if port == "" {
+    port = "8080"
+  }
+  log.Fatal(http.ListenAndServe(":" + port, router))
 }
